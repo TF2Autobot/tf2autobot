a4380cb0d64688c6622bf804f125f5ac8928465e
idinium96 <idin100msc@gmail.com>
2020-09-09 06:57:55 +0800

    add a space after full stop

--- src/classes/MyHandler.ts
+++ src/classes/MyHandler.ts
@@ -1309,13 +1309,13 @@ export = class MyHandler extends Handler {
             this.bot.sendMessage(
                 offer.partner,
                 'I have accepted your offer and the trade will take a while to complete since it is quite a big offer.' +
-                    'If the trade did not complete after 5-10 minutes had passed, please add me and send "!message help accept trade" (without the double quotes).'
+                    ' If the trade did not complete after 5-10 minutes had passed, please add me and send "!message help accept trade" (without the double quotes).'
             );
         } else {
             this.bot.sendMessage(
                 offer.partner,
                 'I have accepted your offer and the trade will be completed in seconds.' +
-                    'If the trade did not complete after 1-2 minutes had passed, please add me and send "!message help accept trade" (without the double quotes).'
+                    ' If the trade did not complete after 1-2 minutes had passed, please add me and send "!message help accept trade" (without the double quotes).'
             );
         }
 
