6e5350b624e6f5485539ff4272752410269ca560
DexSpirit <61100980+Dex-Spirit@users.noreply.github.com>
2020-10-30 19:06:49 +0200

    send CUSTOM_SUCCESS_MESSAGE in the second message

--- src/classes/MyHandler.ts
+++ src/classes/MyHandler.ts
@@ -1635,9 +1635,11 @@ export = class MyHandler extends Handler {
 
         if (handledByUs && offer.data('switchedState') !== offer.state) {
             if (notify) {
-                this.bot.sendMessage(offer.partner, '/pre ✅ Success! The offer went through successfully.');
-                if (process.env.CUSTOM_SUCCESS_MESSAGE) {
-                    this.bot.sendMessage(offer.partner, process.env.CUSTOM_SUCCESS_MESSAGE);
+                if (offer.state === TradeOfferManager.ETradeOfferState.Accepted) {
+                    this.bot.sendMessage(offer.partner, '/pre ✅ Success! The offer went through successfully.');
+                    if (process.env.CUSTOM_SUCCESS_MESSAGE) {
+                        this.bot.sendMessage(offer.partner, process.env.CUSTOM_SUCCESS_MESSAGE);
+                    }
                 } else if (offer.state === TradeOfferManager.ETradeOfferState.InEscrow) {
                     this.bot.sendMessage(
                         offer.partner,
