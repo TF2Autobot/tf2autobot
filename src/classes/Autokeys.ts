d8e9bc9be7a26bc1a660714f9ca6e3b083e1298f
idinium96 <idin100msc@gmail.com>
2020-11-16 06:12:40 +0800

    remove Autokeys investigation note

--- src/classes/Autokeys.ts
+++ src/classes/Autokeys.ts
@@ -106,7 +106,6 @@ export = class Autokeys {
         const currRef = pure.refTotalInScrap;
 
         const keyEntry = this.bot.pricelist.getPrice('5021;6', false);
-        // let us see what you start from here...
 
         const currKeyPrice = this.bot.pricelist.getKeyPrices();
 
