3b846e3a6f68161137c0dd3f8d81696da3dd953e
Joseph Lawson <joe@joekiller.com>
2020-11-25 12:38:20 -0500

    fix option saving

--- src/classes/Commands/optionsCommands.ts
+++ src/classes/Commands/optionsCommands.ts
@@ -1,125 +1,93 @@
 import SteamID from 'steamid';
-import * as fs from 'fs';
+import { promises as fsp } from 'fs';
 import * as path from 'path';
 
 import Bot from '../Bot';
 import CommandParser from '../CommandParser';
-import { JsonOptions } from '../Options';
-// import BotManager from '../BotManager';
+import { JsonOptions, removeCliOptions } from '../Options';
 
 import { deepMerge } from '../../lib/tools/deep-merge';
 import validator from '../../lib/validator';
 
 export function optionsCommand(steamID: SteamID, bot: Bot): void {
-    const optionsPath = path.join(__dirname, `../../../files/${bot.options.steamAccountName}/options.json`);
-
-    fs.readFile(optionsPath, { encoding: 'utf8' }, (err, data) => {
-        if (err) {
-            bot.sendMessage(steamID, '❌ Error reading options.json file from disk: ' + err);
-            return;
-        }
-        const options = JSON.parse(data);
-
-        // remove Discord Webhook URLs
-        delete options.discordWebhook.tradeSummary.url;
-        delete options.discordWebhook.offerReview.url;
-        delete options.discordWebhook.messages.url;
-        delete options.discordWebhook.priceUpdate.url;
-        delete options.discordWebhook.sendAlert.url;
-
-        bot.sendMessage(steamID, `/code ${JSON.stringify(options, null, 4)}`);
-    });
+    const liveOptions = deepMerge({}, bot.options);
+    // remove any CLI stuff
+    removeCliOptions(liveOptions);
+    // remove Discord Webhook URLs
+    delete liveOptions.discordWebhook.tradeSummary.url;
+    delete liveOptions.discordWebhook.offerReview.url;
+    delete liveOptions.discordWebhook.messages.url;
+    delete liveOptions.discordWebhook.priceUpdate.url;
+    delete liveOptions.discordWebhook.sendAlert.url;
+    bot.sendMessage(steamID, `/code ${JSON.stringify(liveOptions, null, 4)}`);
 }
 
 export function updateOptionsCommand(steamID: SteamID, message: string, bot: Bot): void {
     const optionsPath = path.join(__dirname, `../../../files/${bot.options.steamAccountName}/options.json`);
     const params = CommandParser.parseParams(CommandParser.removeCommand(message));
+    const saveOptions = deepMerge({}, bot.options);
+    removeCliOptions(saveOptions);
 
-    fs.readFile(optionsPath, { encoding: 'utf8' }, (err, data) => {
-        if (err) {
-            bot.sendMessage(steamID, '❌ Error reading options.json file from disk: ' + err);
+    if (typeof params.highValue === 'object') {
+        if (params.highValue.sheens !== undefined) {
+            params.highValue.sheens = new Array(saveOptions.highValue.sheens.push(params.highValue.sheens));
+        }
+        if (params.highValue.killstreakers !== undefined) {
+            params.highValue.killstreakers = new Array(
+                saveOptions.highValue.killstreakers.push(params.highValue.killstreakers)
+            );
+            saveOptions.highValue.killstreakers.length = 0;
+        }
+    }
+
+    if (typeof params.manualReview === 'object') {
+        if (params.manualReview.invalidValue.exceptionValue.skus !== undefined) {
+            params.manualReview.invalidValue.exceptionValue.skus = new Array(
+                saveOptions.manualReview.invalidValue.exceptionValue.skus.push(
+                    params.manualReview.invalidValue.exceptionValue.skus
+                )
+            );
+            saveOptions.manualReview.invalidValue.exceptionValue.skus.length = 0;
+        }
+    }
+
+    if (typeof params.discordWebhook === 'object') {
+        if (params.discordWebhook.tradeSummary.url !== undefined) {
+            params.discordWebhook.tradeSummary.url = new Array(
+                saveOptions.discordWebhook.tradeSummary.url.push(params.discordWebhook.tradeSummary.url)
+            );
+            saveOptions.discordWebhook.tradeSummary.url.length = 0;
+        }
+
+        if (params.discordWebhook.tradeSummary.mentionOwner.itemSkus !== undefined) {
+            params.discordWebhook.tradeSummary.mentionOwner.itemSkus = new Array(
+                saveOptions.discordWebhook.tradeSummary.mentionOwner.itemSkus.push(
+                    params.discordWebhook.tradeSummary.mentionOwner.itemSkus
+                )
+            );
+            saveOptions.discordWebhook.tradeSummary.mentionOwner.itemSkus.length = 0;
+        }
+    }
+
+    if (Object.keys(params).length === 0) {
+        bot.sendMessage(steamID, '⚠️ Missing properties to update.');
+        return;
+    }
+
+    const result: JsonOptions = deepMerge(saveOptions, params);
+
+    const errors = validator(result, 'options');
+    if (errors !== null) {
+        bot.sendMessage(steamID, '❌ Error updating options: ' + errors.join(', '));
+        return;
+    }
+    fsp.writeFile(optionsPath, JSON.stringify(saveOptions, null, 4), { encoding: 'utf8' })
+        .then(() => {
+            deepMerge(bot.options, saveOptions);
+            return bot.sendMessage(steamID, '✅ Updated options!');
+        })
+        .catch(err => {
+            bot.sendMessage(steamID, '❌ Error saving options file to disk: ' + err);
             return;
-        }
-
-        const optionsJson = JSON.parse(data) as JsonOptions;
-
-        if (typeof params.highValue === 'object') {
-            if (params.highValue.sheens !== undefined) {
-                params.highValue.sheens = new Array(optionsJson.highValue.sheens.push(params.highValue.sheens));
-            }
-            if (params.highValue.killstreakers !== undefined) {
-                params.highValue.killstreakers = new Array(
-                    optionsJson.highValue.killstreakers.push(params.highValue.killstreakers)
-                );
-                optionsJson.highValue.killstreakers.length = 0;
-            }
-        }
-
-        if (typeof params.manualReview === 'object') {
-            if (params.manualReview.invalidValue.exceptionValue.skus !== undefined) {
-                params.manualReview.invalidValue.exceptionValue.skus = new Array(
-                    optionsJson.manualReview.invalidValue.exceptionValue.skus.push(
-                        params.manualReview.invalidValue.exceptionValue.skus
-                    )
-                );
-                optionsJson.manualReview.invalidValue.exceptionValue.skus.length = 0;
-            }
-        }
-
-        if (typeof params.discordWebhook === 'object') {
-            if (params.discordWebhook.tradeSummary.url !== undefined) {
-                params.discordWebhook.tradeSummary.url = new Array(
-                    optionsJson.discordWebhook.tradeSummary.url.push(params.discordWebhook.tradeSummary.url)
-                );
-                optionsJson.discordWebhook.tradeSummary.url.length = 0;
-            }
-
-            if (params.discordWebhook.tradeSummary.mentionOwner.itemSkus !== undefined) {
-                params.discordWebhook.tradeSummary.mentionOwner.itemSkus = new Array(
-                    optionsJson.discordWebhook.tradeSummary.mentionOwner.itemSkus.push(
-                        params.discordWebhook.tradeSummary.mentionOwner.itemSkus
-                    )
-                );
-                optionsJson.discordWebhook.tradeSummary.mentionOwner.itemSkus.length = 0;
-            }
-        }
-
-        if (Object.keys(params).length === 0) {
-            bot.sendMessage(steamID, '⚠️ Missing properties to update.');
-            return;
-        }
-
-        const result: JsonOptions = deepMerge(optionsJson, params as JsonOptions);
-
-        const errors = validator(result, 'options');
-        if (errors !== null) {
-            bot.sendMessage(steamID, '❌ Error updating options: ' + errors.join(', '));
-            return;
-        }
-
-        fs.writeFile(optionsPath, JSON.stringify(result, null, 4), { encoding: 'utf8' }, err => {
-            if (err) {
-                bot.sendMessage(steamID, '❌ Error saving options file to disk: ' + err);
-                return;
-            }
-
-            // now refresh Options and I don't know how. Right now it will successfully modified the file,
-            // but the bot still use the old Options.
-
-            // const botManager = new BotManager();
-            // botManager.reloadOptions(result);
-            // ^ This crashed the bot:
-
-            /* TypeError: BotManager_1.default is not a constructor
-             *      at Object.updateOptionsCommand (D:\Github\tf2autobot\dist\classes\Commands\optionsCommands.js:85:24)
-             *      at Commands.processMessage (D:\Github\tf2autobot\dist\classes\Commands\Commands.js:203:30)
-             *      at MyHandler.onMessage (D:\Github\tf2autobot\dist\classes\MyHandler\MyHandler.js:284:23)
-             *      at Bot.onMessage (D:\Github\tf2autobot\dist\classes\Bot.js:528:22)
-             *      at Immediate.<anonymous> (D:\Github\tf2autobot\dist\classes\Bot.js:143:21)
-             *      at processImmediate (internal/timers.js:456:21)
-             */
-
-            bot.sendMessage(steamID, '✅ Updated options!');
         });
-    });
 }
--- src/classes/Options.ts
+++ src/classes/Options.ts
@@ -473,6 +473,16 @@ function loadJsonOptions(p: string, options?: Options): JsonOptions {
     return deepMerge(fileOptions, incomingOptions);
 }
 
+export function removeCliOptions(incomingOptions: Options): void {
+    const findNonEnv = validator(incomingOptions, 'options');
+    if (findNonEnv) {
+        findNonEnv
+            .filter(e => e.includes('unknown property'))
+            .map(e => e.slice(18, -1))
+            .map(e => delete incomingOptions[e]);
+    }
+}
+
 export function loadOptions(options?: Options): Options {
     const incomingOptions = options ? deepMerge({}, options) : {};
     const steamAccountName = getOption('steamAccountName', '', String, incomingOptions);
@@ -503,13 +513,7 @@ export function loadOptions(options?: Options): Options {
         debug: getOption('debug', true, JSON.parse, incomingOptions),
         debugFile: getOption('debugFile', true, JSON.parse, incomingOptions)
     };
-    const findNonEnv = validator(incomingOptions, 'options');
-    if (findNonEnv) {
-        findNonEnv
-            .filter(e => e.includes('unknown property'))
-            .map(e => e.slice(18, -1))
-            .map(e => delete incomingOptions[e]);
-    }
+    removeCliOptions(incomingOptions);
     const jsonOptions = loadJsonOptions(
         path.resolve(__dirname, '..', '..', 'files', envOptions.steamAccountName, 'options.json'),
         incomingOptions
