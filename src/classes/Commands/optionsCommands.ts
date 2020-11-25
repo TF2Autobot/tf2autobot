1c94a415bc4b814dbd1047572b05bdffed3e78a8
idinium96 <idin100msc@gmail.com>
2020-11-25 18:30:24 +0800

    use try-catch statement

--- src/classes/Commands/optionsCommands.ts
+++ src/classes/Commands/optionsCommands.ts
@@ -7,18 +7,22 @@ import Bot from '../Bot';
 
 export function optionsCommand(steamID: SteamID, bot: Bot): void {
     const optionsPath = path.join(__dirname, `../../../files/${bot.options.steamAccountName}/options.json`);
-    const optionJson = fs.readFileSync(optionsPath, { encoding: 'utf8' });
+    try {
+        const optionJson = fs.readFileSync(optionsPath, { encoding: 'utf8' });
 
-    const data = JSON.parse(optionJson);
+        const data = JSON.parse(optionJson);
 
-    // remove Discord Webhook URLs
-    delete data.discordWebhook.tradeSummary.url;
-    delete data.discordWebhook.offerReview.url;
-    delete data.discordWebhook.messages.url;
-    delete data.discordWebhook.priceUpdate.url;
-    delete data.discordWebhook.sendAlert.url;
+        // remove Discord Webhook URLs
+        delete data.discordWebhook.tradeSummary.url;
+        delete data.discordWebhook.offerReview.url;
+        delete data.discordWebhook.messages.url;
+        delete data.discordWebhook.priceUpdate.url;
+        delete data.discordWebhook.sendAlert.url;
 
-    bot.sendMessage(steamID, `/code ${JSON.stringify(data, null, 4)}`);
+        bot.sendMessage(steamID, `/code ${JSON.stringify(data, null, 4)}`);
+    } catch (err) {
+        bot.sendMessage(steamID, '❌ Error reading options.json file from disk. Please try again.');
+    }
 }
 
 // export function updateOptionsCommand(steamID: SteamID, message: string, bot: Bot): void {
