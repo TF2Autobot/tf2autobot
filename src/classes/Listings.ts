fd3d853f754b8e37866898efb355f81066362cf0
idinium96 <idin100msc@gmail.com>
2020-12-05 00:19:23 +0800

    🐛 fix bot crash

--- src/classes/Listings.ts
+++ src/classes/Listings.ts
@@ -538,149 +538,153 @@ export default class Listings {
             const sheenName: string[] = [];
             const paintName: string[] = [];
 
-            const descriptions = econ.descriptions;
+            // if econ undefined, then skip because it will make your bot crashed.
+            if (econ !== undefined) {
+                const descriptions = econ.descriptions;
 
-            descriptions.forEach(desc => {
-                const opt = this.bot.options.highValue;
+                descriptions.forEach(desc => {
+                    const opt = this.bot.options.highValue;
 
-                const value = desc.value;
-                const color = desc.color;
+                    const value = desc.value;
+                    const color = desc.color;
 
-                const part = value
-                    .replace('(', '')
-                    .replace(/: \d+\)/g, '')
-                    .trim();
-                const strangePartNames = Object.keys(strangePartsData);
+                    const part = value
+                        .replace('(', '')
+                        .replace(/: \d+\)/g, '')
+                        .trim();
+                    const strangePartNames = Object.keys(strangePartsData);
 
-                if (
-                    value.startsWith('Halloween:') &&
-                    value.endsWith('(spell only active during event)') &&
-                    color === '7ea9d1'
-                ) {
-                    // Show all
-                    hasSpells = true;
-                    const spellName = value.substring(10, value.length - 32).trim();
-                    spellNames.push(
-                        spellName
-                            .replace('Putrescent Pigmentation', 'PP 🍃')
-                            .replace('Die Job', 'DJ 🍐')
-                            .replace('Chromatic Corruption', 'CC 🪀')
-                            .replace('Spectral Spectrum', 'Spec 🔵🔴')
-                            .replace('Sinister Staining', 'Sin 🍈')
-                            .replace('Voices From Below', 'VFB 🗣️')
-                            .replace('Team Spirit Footprints', 'TS-FP 🔵🔴')
-                            .replace('Gangreen Footprints', 'GG-FP 🟡')
-                            .replace('Corpse Gray Footprints', 'CG-FP 👽')
-                            .replace('Violent Violet Footprints', 'VV-FP ♨️')
-                            .replace('Rotten Orange Footprints', 'RO-FP 🍊')
-                            .replace('Bruised Purple Footprints', 'BP-FP 🍷')
-                            .replace('Headless Horseshoes', 'HH 🍇')
-                            .replace('Exorcism', '👻')
-                            .replace('Pumpkin Bomb', '🎃💣')
-                            .replace('Halloween Fire', '🔥🟢')
-                    );
-                } else if (
-                    (part === 'Kills' || part === 'Assists'
-                        ? econ.type.includes('Strange') && econ.type.includes('Points Scored')
-                        : strangePartNames.includes(part)) &&
-                    color === '756b5e'
-                ) {
-                    // Only user specified in highValue.strangeParts
-                    if (opt.strangeParts.includes(part)) {
-                        hasStrangeParts = true;
-                        partsNames.push(part);
-                    }
-                } else if (value.startsWith('Killstreaker: ') && color === '7ea9d1') {
-                    const killstreaker = value.replace('Killstreaker: ', '').trim();
-
-                    // Only user specified in highValue.killstreakers
-                    if (opt.killstreakers.includes(killstreaker)) {
-                        hasKillstreaker = true;
-                        killstreakerName.push(
-                            killstreaker
-                                .replace('Cerebral Discharge', '⚡')
-                                .replace('Fire Horns', '🔥🐮')
-                                .replace('Flames', '🔥')
-                                .replace('Hypno-Beam', '😵💫')
-                                .replace('Incinerator', '🚬')
-                                .replace('Singularity', '🔆')
-                                .replace('Tornado', '🌪️')
+                    if (
+                        value.startsWith('Halloween:') &&
+                        value.endsWith('(spell only active during event)') &&
+                        color === '7ea9d1'
+                    ) {
+                        // Show all
+                        hasSpells = true;
+                        const spellName = value.substring(10, value.length - 32).trim();
+                        spellNames.push(
+                            spellName
+                                .replace('Putrescent Pigmentation', 'PP 🍃')
+                                .replace('Die Job', 'DJ 🍐')
+                                .replace('Chromatic Corruption', 'CC 🪀')
+                                .replace('Spectral Spectrum', 'Spec 🔵🔴')
+                                .replace('Sinister Staining', 'Sin 🍈')
+                                .replace('Voices From Below', 'VFB 🗣️')
+                                .replace('Team Spirit Footprints', 'TS-FP 🔵🔴')
+                                .replace('Gangreen Footprints', 'GG-FP 🟡')
+                                .replace('Corpse Gray Footprints', 'CG-FP 👽')
+                                .replace('Violent Violet Footprints', 'VV-FP ♨️')
+                                .replace('Rotten Orange Footprints', 'RO-FP 🍊')
+                                .replace('Bruised Purple Footprints', 'BP-FP 🍷')
+                                .replace('Headless Horseshoes', 'HH 🍇')
+                                .replace('Exorcism', '👻')
+                                .replace('Pumpkin Bomb', '🎃💣')
+                                .replace('Halloween Fire', '🔥🟢')
                         );
+                    } else if (
+                        (part === 'Kills' || part === 'Assists'
+                            ? econ.type.includes('Strange') && econ.type.includes('Points Scored')
+                            : strangePartNames.includes(part)) &&
+                        color === '756b5e'
+                    ) {
+                        // Only user specified in highValue.strangeParts
+                        if (opt.strangeParts.includes(part)) {
+                            hasStrangeParts = true;
+                            partsNames.push(part);
+                        }
+                    } else if (value.startsWith('Killstreaker: ') && color === '7ea9d1') {
+                        const killstreaker = value.replace('Killstreaker: ', '').trim();
+
+                        // Only user specified in highValue.killstreakers
+                        if (opt.killstreakers.includes(killstreaker)) {
+                            hasKillstreaker = true;
+                            killstreakerName.push(
+                                killstreaker
+                                    .replace('Cerebral Discharge', '⚡')
+                                    .replace('Fire Horns', '🔥🐮')
+                                    .replace('Flames', '🔥')
+                                    .replace('Hypno-Beam', '😵💫')
+                                    .replace('Incinerator', '🚬')
+                                    .replace('Singularity', '🔆')
+                                    .replace('Tornado', '🌪️')
+                            );
+                        }
+                    } else if (value.startsWith('Sheen: ') && color === '7ea9d1') {
+                        const sheen = value.replace('Sheen: ', '').trim();
+
+                        // Only user specified in highValue.sheens
+                        if (opt.sheens.includes(sheen)) {
+                            hasSheen = true;
+                            sheenName.push(
+                                sheen
+                                    .replace('Team Shine', '🔵🔴')
+                                    .replace('Hot-Rod', '🎗️')
+                                    .replace('Manndarin', '🟠')
+                                    .replace('Deadly Daffodil', '🟡')
+                                    .replace('Mean Green', '🟢')
+                                    .replace('Agonizing Emerald', '🟩')
+                                    .replace('Villainous Violet', '🩱')
+                            );
+                        }
+                    } else if (value.startsWith('Paint Color: ') && color === '756b5e') {
+                        const paint = value.replace('Paint Color: ', '').trim();
+
+                        // Only user specified in highValue.painted
+                        if (opt.painted.includes(paint)) {
+                            hasPaint = true;
+                            paintName.push(
+                                paint
+                                    .replace('A Color Similar to Slate', '🧪')
+                                    .replace('A Deep Commitment to Purple', '🪀')
+                                    .replace('A Distinctive Lack of Hue', '🎩')
+                                    .replace("A Mann's Mint", '👽')
+                                    .replace('After Eight', '🏴')
+                                    .replace('Aged Moustache Grey', '👤')
+                                    .replace('An Extraordinary Abundance of Tinge', '🏐')
+                                    .replace('Australium Gold', '🏆')
+                                    .replace('Color No. 216-190-216', '🧠')
+                                    .replace('Dark Salmon Injustice', '🐚')
+                                    .replace('Drably Olive', '🥝')
+                                    .replace('Indubitably Green', '🥦')
+                                    .replace('Mann Co. Orange', '🏀')
+                                    .replace('Muskelmannbraun', '👜')
+                                    .replace("Noble Hatter's Violet", '🍇')
+                                    .replace('Peculiarly Drab Tincture', '🪑')
+                                    .replace('Pink as Hell', '🎀')
+                                    .replace('Radigan Conagher Brown', '🚪')
+                                    .replace('The Bitter Taste of Defeat and Lime', '💚')
+                                    .replace("The Color of a Gentlemann's Business Pants", '🧽')
+                                    .replace('Ye Olde Rustic Colour', '🥔')
+                                    .replace("Zepheniah's Greed", '🌳')
+                                    .replace('An Air of Debonair', '👜🔷')
+                                    .replace('Balaclavas Are Forever', '👜🔷')
+                                    .replace("Operator's Overalls", '👜🔷')
+                                    .replace('Cream Spirit', '🍘🥮')
+                                    .replace('Team Spirit', '🔵🔴')
+                                    .replace('The Value of Teamwork', '👨🏽‍🤝‍👨🏻')
+                                    .replace('Waterlogged Lab Coat', '👨🏽‍🤝‍👨🏽')
+                            );
+                        }
                     }
-                } else if (value.startsWith('Sheen: ') && color === '7ea9d1') {
-                    const sheen = value.replace('Sheen: ', '').trim();
+                });
 
-                    // Only user specified in highValue.sheens
-                    if (opt.sheens.includes(sheen)) {
-                        hasSheen = true;
-                        sheenName.push(
-                            sheen
-                                .replace('Team Shine', '🔵🔴')
-                                .replace('Hot-Rod', '🎗️')
-                                .replace('Manndarin', '🟠')
-                                .replace('Deadly Daffodil', '🟡')
-                                .replace('Mean Green', '🟢')
-                                .replace('Agonizing Emerald', '🟩')
-                                .replace('Villainous Violet', '🩱')
-                        );
-                    }
-                } else if (value.startsWith('Paint Color: ') && color === '756b5e') {
-                    const paint = value.replace('Paint Color: ', '').trim();
+                const opt = this.bot.options.details.highValue;
 
-                    // Only user specified in highValue.painted
-                    if (opt.painted.includes(paint)) {
-                        hasPaint = true;
-                        paintName.push(
-                            paint
-                                .replace('A Color Similar to Slate', '🧪')
-                                .replace('A Deep Commitment to Purple', '🪀')
-                                .replace('A Distinctive Lack of Hue', '🎩')
-                                .replace("A Mann's Mint", '👽')
-                                .replace('After Eight', '🏴')
-                                .replace('Aged Moustache Grey', '👤')
-                                .replace('An Extraordinary Abundance of Tinge', '🏐')
-                                .replace('Australium Gold', '🏆')
-                                .replace('Color No. 216-190-216', '🧠')
-                                .replace('Dark Salmon Injustice', '🐚')
-                                .replace('Drably Olive', '🥝')
-                                .replace('Indubitably Green', '🥦')
-                                .replace('Mann Co. Orange', '🏀')
-                                .replace('Muskelmannbraun', '👜')
-                                .replace("Noble Hatter's Violet", '🍇')
-                                .replace('Peculiarly Drab Tincture', '🪑')
-                                .replace('Pink as Hell', '🎀')
-                                .replace('Radigan Conagher Brown', '🚪')
-                                .replace('The Bitter Taste of Defeat and Lime', '💚')
-                                .replace("The Color of a Gentlemann's Business Pants", '🧽')
-                                .replace('Ye Olde Rustic Colour', '🥔')
-                                .replace("Zepheniah's Greed", '🌳')
-                                .replace('An Air of Debonair', '👜🔷')
-                                .replace('Balaclavas Are Forever', '👜🔷')
-                                .replace("Operator's Overalls", '👜🔷')
-                                .replace('Cream Spirit', '🍘🥮')
-                                .replace('Team Spirit', '🔵🔴')
-                                .replace('The Value of Teamwork', '👨🏽‍🤝‍👨🏻')
-                                .replace('Waterlogged Lab Coat', '👨🏽‍🤝‍👨🏽')
-                        );
-                    }
-                }
-            });
+                if (hasSpells || hasKillstreaker || hasSheen || hasStrangeParts || hasPaint) {
+                    highValueString = ' | ';
 
-            const opt = this.bot.options.details.highValue;
+                    if (hasSpells && opt.showSpells) highValue.spells = `🎃 Spelled: ${spellNames.join(' + ')}`;
+                    if (hasStrangeParts && opt.showStrangeParts)
+                        highValue.parts = `🎰 Parts: ${partsNames.join(' + ')}`;
+                    if (hasKillstreaker && opt.showKillstreaker)
+                        highValue.killstreaker = `🤩 Killstreaker: ${killstreakerName.join(' + ')}`;
+                    if (hasSheen && opt.showSheen) highValue.sheen = `✨ Sheen: ${sheenName.join(' + ')}`;
+                    if (hasPaint && opt.showPainted) highValue.painted = `🎨 Painted: ${paintName.join(' + ')}`;
 
-            if (hasSpells || hasKillstreaker || hasSheen || hasStrangeParts || hasPaint) {
-                highValueString = ' | ';
-
-                if (hasSpells && opt.showSpells) highValue.spells = `🎃 Spelled: ${spellNames.join(' + ')}`;
-                if (hasStrangeParts && opt.showStrangeParts) highValue.parts = `🎰 Parts: ${partsNames.join(' + ')}`;
-                if (hasKillstreaker && opt.showKillstreaker)
-                    highValue.killstreaker = `🤩 Killstreaker: ${killstreakerName.join(' + ')}`;
-                if (hasSheen && opt.showSheen) highValue.sheen = `✨ Sheen: ${sheenName.join(' + ')}`;
-                if (hasPaint && opt.showPainted) highValue.painted = `🎨 Painted: ${paintName.join(' + ')}`;
-
-                for (let i = 0; i < Object.keys(highValue).length; i++) {
-                    if (Object.values(highValue)[i] !== '') {
-                        highValueString += Object.values(highValue)[i] + ' | ';
+                    for (let i = 0; i < Object.keys(highValue).length; i++) {
+                        if (Object.values(highValue)[i] !== '') {
+                            highValueString += Object.values(highValue)[i] + ' | ';
+                        }
                     }
                 }
             }
