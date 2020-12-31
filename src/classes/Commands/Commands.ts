16c9836ba53a7ac4c81d88889cd85e02437b0124
Joseph Lawson <joe@joekiller.com>
2020-12-31 09:34:18 -0500

    🐛 fix sellCart not counting their items

--- src/classes/Commands/Commands.ts
+++ src/classes/Commands/Commands.ts
@@ -401,7 +401,7 @@ export default class Commands {
 
         const cart = Cart.getCart(steamID) || new UserCart(steamID, this.bot);
 
-        const cartAmount = cart.getOurCount(match.sku);
+        const cartAmount = cart.getTheirCount(match.sku);
         const ourAmount = this.bot.inventoryManager.getInventory().getAmount(match.sku);
         const amountCanTrade = this.bot.inventoryManager.amountCanTrade(match.sku, true) - cartAmount;
 
