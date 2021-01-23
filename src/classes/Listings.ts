32468e31b2bfd68ef436e688cf5a0389760fb7c3
idinium96 <idin100msc@gmail.com>
2021-01-23 17:37:50 +0800

    🔨 oopppsss

--- src/classes/Listings.ts
+++ src/classes/Listings.ts
@@ -608,7 +608,7 @@ export default class Listings {
                 .replace(/%keyPrice%/g, '')
                 .replace(/%uses%/g, '');
             if (entry.name === 'Mann Co. Supply Crate Key' && this.bot.handler.autokeys.isEnabled) {
-                details += '[𝐀𝐮𝐭𝐨𝐤𝐞𝐲𝐬] ' + details;
+                details = '[𝐀𝐮𝐭𝐨𝐤𝐞𝐲𝐬] ' + details;
             }
             //
         } else {
