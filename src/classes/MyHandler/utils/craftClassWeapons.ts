a3296268ee21211d4b6ef383c26ca6c78798543f
idinium96 <idin100msc@gmail.com>
2021-01-28 10:16:41 +0800

    🔨 fix too many `Can't handle job`

--- src/classes/MyHandler/utils/craftClassWeapons.ts
+++ src/classes/MyHandler/utils/craftClassWeapons.ts
@@ -21,26 +21,32 @@ export default function craftClassWeapons(bot: Bot): Promise<void> {
 
 function craftEachClassWeapons(bot: Bot, weapons: string[], currencies: { [sku: string]: string[] }): Promise<void> {
     return new Promise(resolve => {
-        weapons.forEach((sku1, i) => {
+        let stopLoop = false;
+
+        for (let i = 0; i < weapons.length; i++) {
             // first loop
             // check if that weapon1 only have 1 in inventory AND it's not in pricelist
-            const isWep1 = currencies[sku1].length === 1 && bot.pricelist.getPrice(sku1, true) === null;
+            const isWep1 = currencies[weapons[i]].length === 1 && bot.pricelist.getPrice(weapons[i], true) === null;
 
-            weapons.forEach((sku2, j) => {
-                // second loop inside first loop, but ignore same index (same weapons)
-                if (j !== i) {
-                    // check if that weapon2 only have 1 in inventory AND it's not in pricelist
-                    const isWep2 = currencies[sku2].length === 1 && bot.pricelist.getPrice(sku2, true) === null;
-                    if (isWep1 && isWep2) {
-                        // if both are different weapons and both wep1 and wep2 conditions are true, call combine function
-                        bot.tf2gc.combineClassWeapon([sku1, sku2]);
-                        // break
-                        return resolve();
-                    }
+            for (let j = 0; j < weapons.length; j++) {
+                if (j === i) {
+                    // second loop inside first loop, but ignore same index (same weapons)
+                    continue;
                 }
-            });
-        });
+                const isWep2 = currencies[weapons[j]].length === 1 && bot.pricelist.getPrice(weapons[j], true) === null;
 
+                if (isWep1 && isWep2) {
+                    // if both are different weapons and both wep1 and wep2 conditions are true, call combine function
+                    bot.tf2gc.combineClassWeapon([weapons[i], weapons[j]]);
+                    stopLoop = true;
+                    break;
+                }
+            }
+
+            if (stopLoop) {
+                break;
+            }
+        }
         return resolve();
     });
 }
