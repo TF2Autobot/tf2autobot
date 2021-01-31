7c806a8f0a469b7e72164ddd5ade5f9c6750d891
idinium96 <idin100msc@gmail.com>
2021-02-01 04:53:26 +0800

    🔨 fix wrong intent

--- src/classes/MyHandler/MyHandler.ts
+++ src/classes/MyHandler/MyHandler.ts
@@ -472,7 +472,7 @@ export default class MyHandler extends Handler {
 
                     if (
                         ([0, 2].includes(entry.intent) && amountCanBuy <= 0) ||
-                        ([0, 1].includes(entry.intent) && amountCanSell <= 0)
+                        ([1, 2].includes(entry.intent) && amountCanSell <= 0)
                     ) {
                         // Ignore items we can't buy or sell
                         return false;
