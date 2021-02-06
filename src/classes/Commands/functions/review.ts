3eb32fcf42634a2b5e8643d203e4b733c341c058
idinium96 <47635037+idinium96@users.noreply.github.com>
2021-02-07 02:22:11 +0800

    🔥 this no need, because it's `8` in polldata

--- src/classes/Commands/functions/review.ts
+++ src/classes/Commands/functions/review.ts
@@ -259,10 +259,6 @@ export async function forceAccept(steamID: SteamID, message: string, bot: Bot): 
         return bot.sendMessage(steamID, 'Offer does not exist. ❌');
     }
 
-    if (state !== TradeOfferManager.ETradeOfferState['Active']) {
-        return bot.sendMessage(steamID, 'Offer is not active. ❌');
-    }
-
     try {
         const offer = await bot.trades.getOffer(offerId);
         bot.sendMessage(steamID, `Force accepting offer...`);
