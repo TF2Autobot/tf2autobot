59792884ce0c76a228c01269fa4e3d36dabd7574
idinium96 <47635037+idinium96@users.noreply.github.com>
2021-02-07 06:26:22 +0800

    😵🥴 not sure why `red` become `purple`

--- src/lib/DiscordWebhook/sendAlert.ts
+++ src/lib/DiscordWebhook/sendAlert.ts
@@ -105,7 +105,7 @@ export default function sendAlert(
     } else if (type === 'autoRemoveIntentSellFailed') {
         title = 'Failed to remove item(s) with intent sell';
         description = msg;
-        color = '8323327'; // red
+        color = '16711680'; // red
     } else if (type === 'autoAddPaintedItems') {
         title = 'Added painted items to sell';
         description = msg;
@@ -113,7 +113,7 @@ export default function sendAlert(
     } else if (type === 'autoAddPaintedItemsFailed') {
         title = 'Failed to add painted items to sell';
         description = msg;
-        color = '8323327'; // red
+        color = '16711680'; // red
     } else if (type.includes('autokeys-')) {
         title =
             type === 'autokeys-failedToDisable'
@@ -122,18 +122,18 @@ export default function sendAlert(
                       type.includes('-bank') ? 'banking' : type.includes('-buy') ? 'buying' : 'selling'
                   } for keys - Autokeys`;
         description = msg;
-        color = '8323327'; // red
+        color = '16711680'; // red
     } else if (type === 'failed-accept') {
         title = 'Failed to accept trade';
         description = msg + `\n\nError:\n${JSON.stringify(err)}`;
         content = items[0]; // offer id
-        color = '8323327'; // red
+        color = '16711680'; // red
     } else if (type === 'failed-processing-offer') {
         title = 'Unable to process an offer';
         description =
             `Offer #${items[1]} with ${items[0]} was unable to process due to some issue with Steam.` +
             ' The offer data received was broken because our side and their side are both empty.';
-        color = '8323327'; // red
+        color = '16711680'; // red
     } else {
         title = 'High Valued Items';
         description = `Someone is trying to take your **${items.join(', ')}** that is not in your pricelist.`;
