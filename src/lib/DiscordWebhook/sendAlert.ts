5036c6a444a23db61982e531a07de374e9fba8f7
idinium96 <47635037+idinium96@users.noreply.github.com>
2021-02-08 09:20:26 +0800

    🔨 add a space

--- src/lib/DiscordWebhook/sendAlert.ts
+++ src/lib/DiscordWebhook/sendAlert.ts
@@ -129,18 +129,18 @@ export default function sendAlert(
         title = 'Failed to accept trade';
         description =
             msg +
-            `\n\nError:[${TradeOfferManager.EResult[(err as CustomError).eresult] as string}](https://steamerrors.com/${
-                (err as CustomError).eresult
-            })`;
+            `\n\nError: [${
+                TradeOfferManager.EResult[(err as CustomError).eresult] as string
+            }](https://steamerrors.com/${(err as CustomError).eresult})`;
         content = items[0]; // offer id
         color = '16711680'; // red
     } else if (type === 'failed-decline') {
         title = 'Failed to decline trade';
         description =
             msg +
-            `\n\nError:[${TradeOfferManager.EResult[(err as CustomError).eresult] as string}](https://steamerrors.com/${
-                (err as CustomError).eresult
-            })`;
+            `\n\nError: [${
+                TradeOfferManager.EResult[(err as CustomError).eresult] as string
+            }](https://steamerrors.com/${(err as CustomError).eresult})`;
         content = items[0]; // offer id
         color = '16711680'; // red
     } else if (type === 'failed-processing-offer') {
