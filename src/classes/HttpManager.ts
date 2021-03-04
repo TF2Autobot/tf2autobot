94d80486cc292e02d605c35b5482248e90863e59
Alex Renoki <rennokki@gmail.com>
2021-03-04 19:53:52 +0200

    Added info when starting the HTTP server.

--- src/classes/HttpManager.ts
+++ src/classes/HttpManager.ts
@@ -43,6 +43,10 @@ export default class HttpManager {
             this.app.listen(this.options.httpApiPort, () => {
                 resolve();
                 log.debug(`HTTP Server started: http://127.0.0.1:${this.options.httpApiPort}`);
+                log.info(`This is NOT a HTTP API used to handle data within the bot.
+                    It is solely for managing the bot programatically by providing healthchecks & uptime details.`);
+                log.info(`Please use the TF2Bot GUI v3+ as the main API source.`);
+                log.info(`https://github.com/TF2Autobot/tf2autobot-gui`);
             });
         });
     }
