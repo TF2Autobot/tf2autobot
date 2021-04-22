cbec0969255ca2295e3457fb5db5c3028696973c
idinium96 <47635037+idinium96@users.noreply.github.com>
2021-04-22 08:43:48 +0800

    🔨 fix test error

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,19 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)']
+    testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+    moduleNameMapper: {
+        '@classes/(.*)': '<rootDir>/src/classes/$1',
+        '@Carts/(.*)': '<rootDir>/src/classes/Carts/$1',
+        '@accepted/(.*)': '<rootDir>/src/classes/MyHandler/offer/accepted/$1',
+        '@notify/(.*)': '<rootDir>/src/classes/MyHandler/offer/notify/$1',
+        '@review/(.*)': '<rootDir>/src/classes/MyHandler/offer/review/$1',
+        '@handlerUtils/(.*)': '<rootDir>/src/classes/MyHandler/utils/$1',
+        '@lib/(.*)': '<rootDir>/src/lib/$1',
+        '@pricer/(.*)': '<rootDir>/src/lib/pricer/$1',
+        '@tools/(.*)': '<rootDir>/src/lib/tools/$1',
+        '@extend/(.*)': '<rootDir>/src/lib/extend/$1',
+        '@DiscordWebhook/(.*)': '<rootDir>/src/lib/DiscordWebhook/$1',
+        '@resources/(.*)': '<rootDir>/src/resources/$1'
+    }
 };
