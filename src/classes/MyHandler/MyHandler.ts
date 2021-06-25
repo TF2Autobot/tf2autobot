a503c84160dbee99dcd2f82220bf1f2fdd6845be
idinium96 <47635037+idinium96@users.noreply.github.com>
2021-06-26 07:54:57 +0800

    🔨🎨 fix broken format

--- src/classes/MyHandler/MyHandler.ts
+++ src/classes/MyHandler/MyHandler.ts
@@ -325,12 +325,9 @@ export default class MyHandler extends Handler {
             const dw = this.opt.discordWebhook.sendAlert;
             const isDwEnabled = dw.enable && dw.url !== '';
 
-            const msg = `All items below has been updated with partial price:\n\n• ${bulkUpdatedPartiallyPriced
-                .map(sku => {
-                    const name = this.bot.schema.getName(SKU.fromString(sku), this.opt.tradeSummary.showProperName);
-                    return `${isDwEnabled ? `[${name}](https://www.prices.tf/items/${sku})` : name} (${sku})`;
-                })
-                .join('\n\n• ')}`;
+            const msg = `All items below has been updated with partial price:\n\n• ${bulkUpdatedPartiallyPriced.join(
+                '\n --- '
+            )}`;
 
             if (this.opt.sendAlert.enable && this.opt.sendAlert.partialPrice.onBulkUpdatePartialPriced) {
                 if (isDwEnabled) {
