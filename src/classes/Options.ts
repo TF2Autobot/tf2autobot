13cc26d9e3c4920873905ed65d4ddae3f6555fe6
idinium96 <47635037+idinium96@users.noreply.github.com>
2021-07-19 08:00:26 +0800

    🔄 set default to `true`

--- src/classes/Options.ts
+++ src/classes/Options.ts
@@ -48,7 +48,7 @@ export const DEFAULTS: JsonOptions = {
         },
         alwaysRemoveItemAttributes: {
             customTexture: {
-                enable: false
+                enable: true
             }
         }
     },
