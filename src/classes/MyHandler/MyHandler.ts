8bdaa094e1e28510284049dba56de9f2667b5821
idinium96 <47635037+idinium96@users.noreply.github.com>
2021-11-04 08:35:28 +0800

    🐛 fix bot not playing game after doing other than accepting an offer

--- src/classes/MyHandler/MyHandler.ts
+++ src/classes/MyHandler/MyHandler.ts
@@ -2220,6 +2220,8 @@ export default class MyHandler extends Handler {
             this.resetSentSummaryTimeout = setTimeout(() => {
                 this.sentSummary = {};
             }, 2 * 60 * 1000);
+        } else {
+            this.bot.client.gamesPlayed(this.opt.miscSettings.game.playOnlyTF2 ? 440 : [this.customGameName, 440]);
         }
     }
 
