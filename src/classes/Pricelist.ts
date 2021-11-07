a1b743b46433ef07e6d77287a2f29ab964d32dfd
Joseph Lawson <joe@joekiller.com>
2021-11-07 16:43:44 -0500

    🐛 fix better custom pricer detection (again)
     - last time I swear

--- src/classes/Pricelist.ts
+++ src/classes/Pricelist.ts
@@ -223,10 +223,10 @@ export default class Pricelist extends EventEmitter {
     }
 
     get isUseCustomPricer(): boolean {
-        return (
-            this.options.customPricerUrl !== '' && // empty == default which is api2.prices.tf
-            this.options.customPricerUrl !== 'https://api.prices.tf' &&
-            this.options.customPricerUrl !== 'https://api2.prices.tf'
+        return !(
+            this.options.customPricerUrl === '' || // empty == default which is api2.prices.tf
+            this.options.customPricerUrl === 'https://api.prices.tf' ||
+            this.options.customPricerUrl === 'https://api2.prices.tf'
         );
     }
 
