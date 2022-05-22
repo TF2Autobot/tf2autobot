3cbf725086f248611e007ece37aa52e59d5f7a98
idinium96 <47635037+idinium96@users.noreply.github.com>
2022-05-23 03:40:24 +0800

    🐛 fix possible crash on websocket TIMEOUT

--- src/lib/pricer/pricestf/prices-tf-socket-manager.ts
+++ src/lib/pricer/pricestf/prices-tf-socket-manager.ts
@@ -48,9 +48,15 @@ export default class PricesTfSocketManager {
         this.ws.addEventListener('error', err => {
             if (err.message === 'Unexpected server response: 401') {
                 log.debug('JWT expired');
-                void this.api.setupToken().then(() => this.ws.reconnect());
+                void this.api
+                    .setupToken()
+                    .then(() => this.ws.reconnect())
+                    .catch(err => {
+                        log.error('Websocket error - setupToken():', err);
+                        // Don't attempt to reconnect
+                    });
             } else {
-                log.error(err);
+                log.error('Websocket error', err?.error);
             }
         });
 
