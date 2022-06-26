cb2ee2bfe48afd773a0043ed51db28d5a5e5eb3d
IdiNium <47635037+idinium96@users.noreply.github.com>
2022-06-26 17:09:55 +0800

    🐛 fix unable to detect "RED ROCK ROSCOE PISTOL"

--- src/lib/extend/item/getSKU.ts
+++ src/lib/extend/item/getSKU.ts
@@ -283,7 +283,7 @@ function getElevatedQuality(item: EconItem, normalizeStrangeAsSecondQuality: boo
         item.hasDescription('Strange Stat Clock Attached') ||
         ((isUnusualHat || isOtherItemsNotStrangeQuality) && isNotNormalized)
     ) {
-        if (paintkit) {
+        if (typeof paintkit === 'number') {
             const hasRarityGradeTag = item.tags?.some(
                 tag => tag.category === 'Rarity' && tag.category_name === 'Grade'
             );
