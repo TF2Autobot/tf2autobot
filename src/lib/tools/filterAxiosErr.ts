da3424314492665469a307e3465f82bb4fd3309f
IdiNium <47635037+idinium96@users.noreply.github.com>
2022-07-15 22:23:58 +0800

    🔨 add response `data`

--- src/lib/tools/filterAxiosErr.ts
+++ src/lib/tools/filterAxiosErr.ts
@@ -1,9 +1,15 @@
 import { AxiosError } from 'axios';
 
-export default function filterAxiosErr(err: AxiosError): { message: string; code: string; status: string } {
+export default function filterAxiosErr(err: AxiosError): {
+    message: string;
+    code: string;
+    status: string;
+    data: unknown;
+} {
     return {
         message: err.message,
         code: err.code,
-        status: err.status
+        status: err.status,
+        data: err.response.data
     };
 }
