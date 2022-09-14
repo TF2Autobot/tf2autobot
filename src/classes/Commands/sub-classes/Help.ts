8f0344766a7117bc46016eec20fb3036c1cde90c
Tolfx <matteus@tolfx.dev>
2022-09-14 20:31:49 +0200

    fix: fixed missing \`\` on help command for prefix

--- src/classes/Commands/sub-classes/Help.ts
+++ src/classes/Commands/sub-classes/Help.ts
@@ -100,7 +100,7 @@ export default class HelpCommands {
                         `${prefix}withdraw (sku|name|defindex)=<a>&amount=<number> - Withdraw items.`,
                         `${prefix}withdrawMptf [max=<number>] - [Exclusive Marketplace.tf Sellers] Withdraw items that does not exist on Marketplace.tf Dashboard items.`,
                         `${prefix}expand craftable=(true|false) - Use Backpack Expanders to increase the bot's inventory limit.`,
-                        '${prefix}use (sku|assetid)=<a> - Use an item (such as Gift-Stuffed Stocking 2020 - sku: 5923;6;untradable).',
+                        `${prefix}use (sku|assetid)=<a> - Use an item (such as Gift-Stuffed Stocking 2020 - sku: 5923;6;untradable).`,
                         `${prefix}delete (sku|assetid)=<a> - Delete an item from the bot's inventory (SKU input only) 🚮`,
                         `${prefix}message <steamid> <your message> - Send a message to a specific user 💬`,
                         `${prefix}block <steamid> [reason] - Block a specific user, reason will be saved for future reference.`,
