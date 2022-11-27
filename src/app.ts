4463a84a4f4d896d094853bd2c51d196e7e4dd45
IdiNium <47635037+idinium96@users.noreply.github.com>
2022-11-27 14:34:44 +0800

    🚮 remove debug log

--- src/app.ts
+++ src/app.ts
@@ -33,8 +33,6 @@ const paths = genPaths(options.steamAccountName);
 import log, { init } from './lib/logger';
 init(paths, options);
 
-log.debug('options.discordWebhook.sendTf2Events check', options.discordWebhook.sendTf2Events);
-
 if (process.env.pm_id === undefined && process.env.DOCKER === undefined) {
     log.warn(
         "You are not running the bot with PM2! If the bot crashes it won't start again." +
