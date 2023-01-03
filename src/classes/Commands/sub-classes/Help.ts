46633d2dda931e206f4503392683b53aa9eb123b
Tolfx <matteus@tolfx.dev>
2023-01-03 20:34:08 +0100

    feat: fixed prefix

--- src/classes/Commands/sub-classes/Help.ts
+++ src/classes/Commands/sub-classes/Help.ts
@@ -240,16 +240,16 @@ export default class HelpCommands {
 
     howToTradeCommand(steamID: SteamID): void {
         const custom = this.bot.options.commands.how2trade.customReply.reply;
-
+        const prefix = this.bot.getPrefix(steamID);
         this.bot.sendMessage(
             steamID,
             custom
                 ? custom
                 : '/quote You can either send me an offer yourself, or use one of my commands to request a trade. ' +
-                      'Say you want to buy a Team Captain, just type "${prefix}buy Team Captain", if want to buy more, ' +
-                      'just add the [amount] - "${prefix}buy 2 Team Captain". Type "${prefix}help" for all the commands.' +
-                      '\nYou can also buy or sell multiple items by using the "${prefix}buycart [amount] <item name>" or ' +
-                      '"${prefix}sellcart [amount] <item name>" commands.'
+                      `Say you want to buy a Team Captain, just type "${prefix}buy Team Captain", if want to buy more, ` +
+                      `just add the [amount] - "${prefix}buy 2 Team Captain". Type "${prefix}help" for all the commands.` +
+                      `\nYou can also buy or sell multiple items by using the "${prefix}buycart [amount] <item name>" or ` +
+                      `"${prefix}sellcart [amount] <item name>" commands.`
         );
     }
 }
