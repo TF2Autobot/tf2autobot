aa19c7a1f990160e8d0cd89e8d0fa85e32121858
Tolfx <matteus@tolfx.dev>
2023-01-17 14:45:29 +0100

    feat: created how2trade command (#1475)

---
+++ src/classes/Commands/commands/information/how2trade.ts
@@ -0,0 +1,30 @@
+import SteamID from 'steamid';
+import { ICommand } from '../../CommandHandler';
+import Bot from 'src/classes/Bot';
+import IPricer from 'src/classes/IPricer';
+
+export default class How2TradeCommand implements ICommand {
+    name = 'how2trade';
+
+    description = 'Guide on how to trade with the bot.';
+
+    constructor(public readonly bot: Bot, public readonly pricer: IPricer) {
+        this.bot = bot;
+        this.pricer = pricer;
+    }
+
+    execute = (steamID: SteamID, message: string): void => {
+        const custom = this.bot.options.commands.how2trade.customReply.reply;
+        const prefix = this.bot.getPrefix(steamID);
+        this.bot.sendMessage(
+            steamID,
+            custom
+                ? custom
+                : '/quote You can either send me an offer yourself, or use one of my commands to request a trade. ' +
+                      `Say you want to buy a Team Captain, just type "${prefix}buy Team Captain", if want to buy more, ` +
+                      `just add the [amount] - "${prefix}buy 2 Team Captain". Type "${prefix}help" for all the commands.` +
+                      `\nYou can also buy or sell multiple items by using the "${prefix}buycart [amount] <item name>" or ` +
+                      `"${prefix}sellcart [amount] <item name>" commands.`
+        );
+    };
+}
