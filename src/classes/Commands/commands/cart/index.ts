d342506a460537549416928a64be5570e23c29f2
Tolfx <matteus@tolfx.dev>
2023-01-17 15:44:23 +0100

    feat: created default export for cart commands (#1475)

---
+++ src/classes/Commands/commands/cart/index.ts
@@ -0,0 +1,17 @@
+import BuyAndSellCommand from './BuyAndSell';
+import BuyCartCommand from './BuyCart';
+import CancelCommand from './Cancel';
+import CartCommand from './Cart';
+import CheckoutCommand from './Checkout';
+import ClearCartCommand from './ClearCart';
+import SellCartCommand from './SellCart';
+
+export default [
+    CartCommand,
+    CheckoutCommand,
+    SellCartCommand,
+    ClearCartCommand,
+    CancelCommand,
+    BuyCartCommand,
+    BuyAndSellCommand
+];
