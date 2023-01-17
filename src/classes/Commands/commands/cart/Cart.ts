555c33e1467e9271abb150ca7159d7e2bd9eee4b
Tolfx <matteus@tolfx.dev>
2023-01-17 15:43:15 +0100

    feat: created cart command (#1475)

---
+++ src/classes/Commands/commands/cart/Cart.ts
@@ -0,0 +1,44 @@
+import SteamID from 'steamid';
+import CommandHandler, { ICommand } from '../../CommandHandler';
+import Cart from '../../../Carts/Cart';
+import Bot from '../../../Bot';
+import IPricer from '../../../IPricer';
+
+export default class CartCommand implements ICommand {
+    name = 'cart';
+
+    description = 'View your cart 🛒';
+
+    allowInvalidType = false;
+
+    constructor(
+        public readonly bot: Bot,
+        public readonly pricer: IPricer,
+        public readonly commandHandler: CommandHandler
+    ) {
+        this.bot = bot;
+        this.pricer = pricer;
+        this.commandHandler = commandHandler;
+    }
+
+    execute = (steamID: SteamID, message: string) => {
+        const prefix = this.bot.getPrefix(steamID);
+        const opt = this.bot.options.commands.cart;
+
+        if (!opt.enable) {
+            if (!this.bot.isAdmin(steamID)) {
+                const custom = opt.customReply.disabled;
+                return this.bot.sendMessage(steamID, custom ? custom : '❌ This command is disabled by the owner.');
+            }
+        }
+
+        if (this.commandHandler.isDonating) {
+            return this.bot.sendMessage(
+                steamID,
+                `You're about to send donation. Send "${prefix}donatecart" to view your donation cart summary or "${prefix}donatenow" to send donation now.`
+            );
+        }
+
+        this.bot.sendMessage(steamID, Cart.stringify(steamID, false, prefix));
+    };
+}
