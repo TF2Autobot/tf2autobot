fa7111fd58e9a0c508ca2d527dff55b798575855
Tolfx <matteus@tolfx.dev>
2023-01-18 11:07:25 +0100

    feat: updated index information exporter (#1475)

--- src/classes/Commands/commands/information/index.ts
+++ src/classes/Commands/commands/information/index.ts
@@ -1,6 +1,8 @@
 import HelpCommand from './Help';
+import LinkCommands from './Links';
+import MiscCommands from './Miscs';
 import PriceCommand from './Price';
 import QueueCommand from './Queue';
 import How2TradeCommand from './how2trade';
 
-export default [HelpCommand, How2TradeCommand, PriceCommand, QueueCommand];
+export default [HelpCommand, How2TradeCommand, PriceCommand, QueueCommand, MiscCommands, LinkCommands];
