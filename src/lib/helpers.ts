6b2711044b3366c333f062b941274432c3ba9a4f
TryHardDo <levai.levente2002@gmail.com>
2023-06-26 13:54:11 +0200

    Adding Helper class to helpers.ts

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,3 +1,4 @@
+import EasyCopyPaste from 'easycopypaste';
 import { UnknownDictionaryKnownValues } from '../types/common';
 
 export function exponentialBackoff(n: number, base = 1000): number {
@@ -9,6 +10,27 @@ export function parseJSON(json: string): UnknownDictionaryKnownValues | null {
         return JSON.parse(json) as UnknownDictionaryKnownValues;
     } catch (err) {
         return null;
+    }
+}
+
+export default class Helper {
+    private readonly ecp = new EasyCopyPaste();
+
+    public getEasyCopyPasteStr(itemName: string, intent: 'buy' | 'sell'): string {
+        let intentStr = '';
+        if (intent === 'buy') {
+            intentStr = 'sell_';
+        } else {
+            intentStr = 'buy_';
+        }
+
+        const easyCopyPasteString = this.ecp.toEasyCopyPasteString(itemName);
+
+        return `${intentStr}${easyCopyPasteString}`;
+    }
+
+    public getNormalizedItemName(easyCopyPasteString: string): string {
+        return this.ecp.fromEasyCopyPasteString(easyCopyPasteString);
     }
 }
 
