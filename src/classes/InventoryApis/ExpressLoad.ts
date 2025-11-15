c7d8b8dbb44e1f9710e13a5718ccf250a027aad5
OliverPerring <oliverperring@gmail.com>
2025-11-15 21:03:23 +0000

    Express load added based on pr 1792 from autobot repo

---
+++ src/classes/InventoryApis/ExpressLoad.ts
@@ -0,0 +1,28 @@
+import { UnknownDictionaryKnownValues } from 'src/types/common';
+import Bot from '../Bot';
+import InventoryApi from './InventoryApi';
+
+export default class ExpressLoad extends InventoryApi {
+    constructor(bot: Bot) {
+        super(bot, 'expressLoad');
+    }
+
+    protected getURLAndParams(
+        steamID: string,
+        appID: number,
+        contextID: string
+    ): [string, UnknownDictionaryKnownValues] {
+        return [
+            `https://api.express-load.com/inventory/${steamID}/${appID}/${contextID}`,
+            {}
+        ];
+    }
+
+    protected getHeaders(): UnknownDictionaryKnownValues {
+        return {
+            'X-API-KEY': this.getApiKey(),
+            'User-Agent': 'TF2Autobot'
+        };
+    }
+
+}
