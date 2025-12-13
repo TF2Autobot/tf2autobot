583875efb8549ae30750a8c681273418c73e726e
OliverPerring <oliverperring@gmail.com>
2025-12-13 22:48:05 +0000

    Add placeholder SKU tracking for API trades

--- src/classes/Carts/ApiCart.ts
+++ src/classes/Carts/ApiCart.ts
@@ -18,11 +18,11 @@ export default class ApiCart extends Cart {
         if (!partnerMatch) {
             throw new Error('Invalid trade URL: missing partner parameter');
         }
-        
+
         // Convert account ID to SteamID using the proper format [U:1:accountId]
         const accountId = parseInt(partnerMatch[1]);
         const partnerSteamID = new SteamID(`[U:1:${accountId}]`);
-        
+
         // Use 4-arg constructor with correct partner SteamID
         super(partnerSteamID, bot, [], []);
         this.tradeUrl = tradeUrl;
