8d2aa0d76cdc5df98aa8bead1521bf4f19f3871b
idinium96 <47635037+idinium96@users.noreply.github.com>
2026-07-11 01:03:17 +0800

    🔨 satisfy npm run test

--- src/classes/Trades.ts
+++ src/classes/Trades.ts
@@ -1572,7 +1572,7 @@ export default class Trades {
                                     )}s (${rateLimitAttempts}/${STEAM_RETRY_ATTEMPTS})`
                                 );
 
-                                timersPromises.setTimeout(delay).then(attemptEscrowCheck);
+                                void timersPromises.setTimeout(delay).then(attemptEscrowCheck);
                                 return;
                             }
 
