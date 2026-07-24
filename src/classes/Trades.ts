e5f8ec0fcdb206403025b26309ad28df7139527d
idinium96 <47635037+idinium96@users.noreply.github.com>
2026-07-24 21:27:00 +0800

    🔨 only check if more than 10 seconds

--- src/classes/Trades.ts
+++ src/classes/Trades.ts
@@ -351,7 +351,7 @@ export default class Trades {
                             this.bot.manager.pollInterval = 10 * 1000;
                             const now = dayjs();
                             const timeDiffInMs = now.diff(this.bot.lastTimeCallingDoPoll);
-                            if (timeDiffInMs === 0 || timeDiffInMs >= 10000) {
+                            if (timeDiffInMs >= 10000) {
                                 // Make sure to call doPoll only if first time or last call is more than or equal to 10 seconds
                                 this.bot.lastTimeCallingDoPoll = now.toDate();
                                 this.bot.manager.doPoll();
