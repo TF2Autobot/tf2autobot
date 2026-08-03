ae04009735022dc3611dbd39d1a76756f6acfb91
idinium96 <47635037+idinium96@users.noreply.github.com>
2026-08-03 10:16:29 +0800

    ✅ set minimumPollInterval, disable globalAssetCache (unused), default assetCacheMaxItems

--- src/classes/Carts/CartQueue.ts
+++ src/classes/Carts/CartQueue.ts
@@ -1,6 +1,5 @@
 import SteamID from 'steamid';
 import * as inspect from 'util';
-import dayjs from 'dayjs';
 import pluralize from 'pluralize';
 import Cart from './Cart';
 import Bot from '../Bot';
@@ -201,15 +200,7 @@ export default class CartQueue {
 
             log.debug('pollInterval re-enabled.');
             this.bot.manager.pollInterval = 10 * 1000;
-            const now = dayjs();
-            const timeDiffInMs = now.diff(this.bot.lastTimeCallingDoPoll);
-            if (timeDiffInMs >= 10000) {
-                // Make sure to call doPoll only if first time or last call is more than or equal to 10 seconds
-                this.bot.lastTimeCallingDoPoll = now.toDate();
-                log.debug('doPoll called.');
-                this.bot.manager.doPoll();
-            }
-
+            this.bot.manager.doPoll();
             return;
         }
 
